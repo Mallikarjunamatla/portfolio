494d37114cecd03ef78c1e1be8c251d12a40e937
Mallikarjuna M <42877673+Mallikarjunamatla@users.noreply.github.com>
2021-09-05 10:20:11 +0530

    files added

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+      
+
+function App() {
+  return (
+    <div className="App">
+        
+    <nav class="navigation container">
+      <div class="nav-brand">Dev Abhi</div>
+      <ul class="nav-pills">
+        <li class="list-item-inline">
+          <a href="/" class="link link-active">Home</a>
+        </li>
+        <li class="list-item-inline">
+          <a href="/projects.html" class="link">Projects</a>
+        </li>
+        <li class="list-item-inline">
+          <a href="/blogs.html" class="link">Blogs</a>
+        </li>
+        <li class="list-item-inline">
+          <a href="/about.html" class="link">About</a>
+        </li>
+      </ul>
+    </nav>
+   
+    </div>
+  );
+}
+
+export default App;
